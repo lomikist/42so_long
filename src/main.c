8edc8a13d2f49fd52c0ae6b36a099e125599ccc4
Arman Sargsyan <arsargsy@c1r2s5.42yerevan.am>
2024-03-04 22:47:46 +0400

    not working fully

--- src/main.c
+++ src/main.c
@@ -34,42 +34,52 @@ char	**read_map(int fd)
 // }
 int check_path(char **map, int y, int x )
 {
-	// up
+	//up
 	if (map[y - 1][x] == 'E')
 		return (1);
 	else if (map[y + 1][x] == 'E')
 		return (1);
 	else if (map[y][x + 1] == 'E')
-		return (1);	
+		return (1);
 
 	if (map[y - 1][x] == '0')
 		check_path(map, y - 1, x);
-	else if (map[y + 1][x] == '0')
-		check_path(map, y + 1, x);
 	else if (map[y][x + 1] == '0')
+		check_path(map, y, x + 1);
+	else if (map[y + 1][x] == '0')
+	{
+		map[y][x] = '3';
 		check_path(map, y + 1, x);
+	}
+	else if (map[y][x - 1] == '0')
+	{
+		map[y][x] = '3';
+		check_path(map, y, x - 1);
+	}
 	else
-		return 0;
+		return (0);
+	return 0;
 }
 int	check_lines(char **map)
 {
-	int	i;
+	int		i;
 	size_t	len;
 
 	i = 1;
-	len = ft_strlen(map[0]); 
+	len = ft_strlen(map[0]);
 	while (map[i])
 	{
 		if (ft_strlen(map[i]) != len)
-			return 0;
+			return (0);
 		i++;
 	}
-	return 1;
+	return (1);
 }
 
 int	main(void)
 {
 	char	**map;
+	char	*finded_char;
 	int		fd;
 	int		i;
 
@@ -78,14 +88,13 @@ int	main(void)
 	map = read_map(fd);
 	while (map[i])
 	{
-		if (ft_strchr((const char*)map[i], 'P'))
-		{
-		}
+		finded_char = ft_strchr((const char*)map[i], 'P');
+		if (finded_char)
+			printf("%d", check_path(map, i, finded_char - &map[i][0]));
 		i++;
 	}
-	
 	// check_lines(map);
-	printf("%d",check_lines(map));
 	// check_border(map);
 	close(fd);
+	return (0);
 }
